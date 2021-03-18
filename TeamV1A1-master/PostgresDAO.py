df0727d5e3f03e4e7e4f2d1197b72038bde0649c
sbeetsma <74861186+sbeetsma@users.noreply.github.com>
2021-03-18 17:17:00 +0100

    Update PostgresDAO.py
    removed alot of code not needed in this assignment

--- TeamV1A1-master/PostgresDAO.py
+++ TeamV1A1-master/PostgresDAO.py
@@ -1,12 +1,4 @@
 import psycopg2
-import psycopg2.extras
-import random
-if __name__ == "__main__": #for formatieve opdracht 2c
-    import MongodbDAO
-    import pymongo
-
-
-
 
 #Vul hier je eigen PostgreSQL credentials in:
 host = "localhost"
@@ -74,6 +66,16 @@ class PostgreSQLdb:
         Sets self.cursor to none."""
         self.cursor.close()
         self.cursor = None
+
+    def connect_and_cursor(self, connect_disconnect):
+        """Connects to the database and summons cursor, or closes cursor and closes database connection
+        by using the existing functions for (dis)connecting and closing/summoning a cursor"""
+        if connect_disconnect == 'connect':
+            self.connect()
+            self.summon_cursor()
+        elif connect_disconnect == 'disconnect':
+            self.close_cursor()
+            self.close_connection()
 
     def bare_query(self, query: str, parameters: tuple = None):
         """Executes an SQL query in the database.
@@ -157,118 +159,58 @@ class PostgreSQLdb:
             if query != "":
                 self.query(query + ";", commit_changes=True)
 
-    def method_execute_batch(self, query: str, data_list: list[tuple]):
-        self.connect()
-        self.summon_cursor()
-        psycopg2.extras.execute_batch(self.cursor, query, data_list)
-        self.commit_changes()
-        self.close_cursor()
-        self.close_connection()
-
-
-
-
-#functie voor opdracht 1 voor summatieve opdracht 2c:
-def add_items_to_database(db: PostgreSQLdb):
-    """makes a connection to the PostgreSQL database.
-    calls the regenerate function, that drops the existing tables from the database, and creates empty new ones.
-    loops trough items in range(amount) from the MongoDB database and adds them to the PostgreSQL database.
-
-    parameters:
-        db: the postgres database to fill."""
-
-    db.regenerate_db("DDL1.txt")
-
-    products = MongodbDAO.getDocuments("products")
-    for i in range(20):
-        product_name = products[i]["name"]
-        product_id = products[i]["_id"]
-        product_price = products[i]["price"]["selling_price"]
-        db.query("INSERT INTO Products (product_id, product_name, selling_price) VALUES (%s, %s, %s);",
-                 (product_id, product_name, product_price), commit_changes=True)
-
-# functie voor opdracht 2 voor summatieve opdracht 2c:
-def alle_product_ids(db: PostgreSQLdb):
-    """Functie die alle product ids returned in een lijst uit de relationele database
-
-    parameters:
-        db: de database om ids uit te halen."""
-    product_ids = []
-    products = db.query("SELECT * FROM products", expect_return=True)
-    for product in products:
-        product_ids.append(product[0])
-    return product_ids
-
-def product_id_lijst_input():
-    """Functie voor het verkrijgen van een product id lijst doormiddel van user input.
-    Een lijst van alle opgegeven product ids wordt returned"""
-    product_ids = []
-    while True:
-        productid = input('input een product id, input ok als je je klaar bent met ids invoeren')
-        # als input ok is stop dan met id's vragen
-        if productid == 'ok':
-            break
-        # append id aan product ids lijst
-        product_ids.append(productid)
-    return product_ids
-
-def gemiddelde_prijs(id_lijst, db: PostgreSQLdb):
-    """Functie die de gemiddelde prijs van een gegeven lijst product ids berekent en deze returned (in eurocent)
-    args:
-        id_lijst: lijst van product ids waarvan het gemiddelde berekent wordt
-        db: de database om te doorzoeken"""
-    prijs_lijst = []
-    for id in id_lijst:
-        product = db.query(f"SELECT selling_price FROM products WHERE product_id = '{id}'", expect_return=True)
-        # index 0 is tuple met alle informatie, daarvan index 2 is de prijs
-        prijs_lijst.append(product[0][0])
-    gemiddeld = sum(prijs_lijst) / len(prijs_lijst)
-    return round(gemiddeld)
-
-
-#functie voor opdracht 3 summatieve opdracht 2c:
-def max_abs_price(db: PostgreSQLdb) -> tuple[str]:
-    """Retrieves all products in the database.
-    Selects a random product from that database.
-    Searches through all products for the product with biggest price difference compared to that one.
-
-    Args:
-        db: the database to retrieve products from.
-
-    Returns:
-        tuple containing:
-            0: product_id of the product with the biggest price difference.
-            1: product_id of the randomly selected product.
-    """
-    sql_query = "SELECT product_id, selling_price FROM Products;"
-    products = db.query(sql_query, expect_return=True)
-    random_product = random.choice(products)
-    biggest_dif_product, biggest_dif = None, -1
-    for product in products:
-        dif = abs(random_product[1] - product[1])
-        if dif > biggest_dif:
-            biggest_dif_product, biggest_dif = product, dif
-    return biggest_dif_product[0], random_product[0]
 
 # connect met database
 db = PostgreSQLdb(host, database, user, password, port)
 
+def construct_query_where_and(_select, _from, _where):
+    """"Function to construct a select X from Y where att1 = %s or att2 = %s etc...  works with only 1 attribute with no maximum limit for amount of attributes
+    args:
+        _select: select what_ ex: id
+        _from: from where ex: products
+        _where: list of attributes(names not values).
+    returns:
+        query: ex multiple attributes: SELECT id FROM products WHERE category = %s and targetaudience = %s"""
+
+    query = f"SELECT {_select} FROM {_from} WHERE "
+    # for each attribute add to the query "attribute = %s or "
+    for attribute in _where:
+        query += f"{attribute} = %s or "
+    # remove the "or " at the end of the query
+    query = query[:-4]
+    return query
 
 
-"""
-#function calls for summatieve opdracht 2c:
-print("1:", end="\n\n")
-print(f"Adding items to PostGreSQL...")
-add_items_to_database(db)
-print("Done!")
-
-print("2:", end="\n\n")
-print(f"De gemiddelde prijs van alle producten in PostGreSQL is {gemiddelde_prijs(alle_product_ids(db), db)}.")
-print("Vul ID's in van producten waarvan je de gemiddelde prijs wilt weten. Vul 'ok' in als je klaar bent.")
-print(gemiddelde_prijs(product_id_lijst_input(), db))
-
-print("3:", end="\n\n")
-map = max_abs_price(db)
-print(f"ID van gekozen willikeurig product: {map[1]}")
-print(f"ID van grootste absolute prijsverschil met dat product in PostGreSQL: {map[0]}")
-"""
+def create_rec_table_query(db, table_name, attributes_datatypes):
+    """Function to construct and execute a query for table creation where the product recommendations will be stored
+    args:
+        db: database object where to create the table. db is a object from PostgresDAO
+        table_name: the name of the table that will be created. the table name will be the name of the recommendation rule
+        attributes_datatypes:   the attributes which the recommendation engine will use to fill the tables and their data type ex: 'category VARCHAR, targetaudience VARCHAR,'
+                                all attributes and datatypes must be stored in a single string"""
+    table_query = f"""
+    DROP TABLE IF EXISTS {table_name};
+    CREATE TABLE {table_name}
+    (
+    {attributes_datatypes}
+    pro1 VARCHAR,
+    pro2 VARCHAR,
+    pro3 VARCHAR,
+    pro4 VARCHAR,
+    FOREIGN KEY (pro1)
+    REFERENCES
+    products(id),
+    FOREIGN
+    KEY(pro2)
+    REFERENCES
+    products(id),
+    FOREIGN
+    KEY(pro3)
+    REFERENCES
+    products(id),
+    FOREIGN
+    KEY(pro4)
+    REFERENCES
+    products(id)
+    );"""
+    db.query(table_query, (attributes_datatypes,), commit_changes=True)
